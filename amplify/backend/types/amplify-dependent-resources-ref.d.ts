2c5193139c6a4978edafa7fcc5bcefe905b04376
Lucas Tobon <lucas.tobon.v@gmail.com>
2023-05-11 21:50:16 -0500

    Basic Model was created and a preliminary draft README

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "fixitpro": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
