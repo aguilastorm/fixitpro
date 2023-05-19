a0654e8632f64f18ea0ec078c0b4620dcba19b26
Lucas Tobon <lucas.tobon.v@gmail.com>
2023-05-18 19:35:16 -0500

    Amplify import issue library fixed

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:amplify_api/amplify_api.dart';
 import 'package:fixitpro/fixitpro_app.dart';
 import 'package:flutter/material.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
-import 'amplifyconfiguration.dart';
+// import 'amplifyconfiguration.dart';
 
 import 'models/ModelProvider.dart';
 
